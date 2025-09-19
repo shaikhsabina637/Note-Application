42fa6d2154dee3b9a4a3d1586b5dbd7f410afc77
sabina <shaikhsabina637@gmail.com>
2025-09-19 23:24:15 +0530

    updated the reset passowrd page

--- note-frontend/src/app/resetpassword/page.jsx
+++ note-frontend/src/app/resetpassword/page.jsx
@@ -1,10 +1,13 @@
 "use client"
 import ResetPasswordPage from "@/components/authentication/resetPasswordPage";
-
+import Spinner from "@/components/common/spinner";
+import { Suspense } from "react";
 export default function page() {
   return (
     <>
-    <ResetPasswordPage/>
+     <Suspense fallback={<div className="min-h-screen flex items-center justify-center"><Spinner /></div>}>
+          <ResetPasswordPage/>
+        </Suspense>
     </>
   );
 }
